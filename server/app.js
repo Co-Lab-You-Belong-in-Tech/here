8373812982c3e6d18600f2a3fe8d3deda2809f4e
glewman <77811396+glewman@users.noreply.github.com>
2021-08-11 18:21:09 -0500

    express renders on localhost

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-const { resolve } = require("dns/promises");
 const express = require("express");
+
 
 const app = express();
 app.listen(5050);
 
 app.get("/", (req, res) => {
-    res.send('<p>Home Page</p>')
+    res.send('<p>Home Page</p>');
 });
