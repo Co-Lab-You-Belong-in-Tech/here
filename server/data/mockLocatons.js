af2e14bb78c6f6b8f2a19cc603e3278e87ea3334
glewman <77811396+glewman@users.noreply.github.com>
2021-08-18 12:56:45 -0500

    added mock data for locations and reviews

---
+++ server/data/mockLocatons.js
@@ -0,0 +1,162 @@
+const locations = [{
+'1': {
+    locationId: 1,
+    locationName: 'The Chop House',
+    locationType: 'Steak House',
+    closeIndicator: false,
+    closeDate: '',
+    locationAddress: ''
+},
+'2':{
+    locationId: 2,
+    locationName: 'Skeeters',
+    locationType: 'Bar',
+    closeIndicator: true,
+    closeDate: '',
+    locationAddress: ''
+},
+'3':{
+locationId: 3,
+    locationName: 'Bartolinos Italian Restaurant',
+    locationType: 'Italian Restaurant',
+    closeIndicator: false,
+    closeDate: '',
+    locationAddress: ''
+},
+'4':{
+    locationId: 4,
+        locationName: 'O Reillys Pub',
+        locationType: 'Bar',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'5': {
+    locationId: 5,
+        locationName: 'The Lobster Hut',
+        locationType: 'Seafood Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },  
+'6': {
+    locationId: 6,
+        locationName: 'Carmines',
+        locationType: 'Seafood Restaurant',
+        closeIndicator: true,
+        closeDate: '',
+        locationAddress: ''
+    },  
+'7': {
+    locationId: 7,
+        locationName: 'Pappys BBQ',
+        locationType: 'BBQ Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'8': {
+    locationId: 8,
+        locationName: 'Genos Steaks',
+        locationType: 'Cheese Steak Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'9': {
+    locationId: 9,
+        locationName: 'Uno Pizzaria',
+        locationType: 'Pizza Restaurant',
+        closeIndicator: true,
+        closeDate: '',
+        locationAddress: ''
+    },
+'10': {
+    locationId: 10,
+        locationName: 'Hunan Star',
+        locationType: 'Chinese Food Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'11': {
+    locationId: 11,
+        locationName: 'Centerfield',
+        locationType: 'Bar',
+        closeIndicator: true,
+        closeDate: '',
+        locationAddress: ''
+    },
+'12':{
+    locationId: 12,
+        locationName: 'The Office',
+        locationType: 'Bar',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'13': {
+    locationId: 13,
+        locationName: 'Sams Steakhouse',
+        locationType: 'Steakhouse',
+        closeIndicator: true,
+        closeDate: '',
+        locationAddress: ''
+    },
+'14': {
+    locationId: 14,
+        locationName: 'Margaritas',
+        locationType: 'Mexican Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'15': {
+    locationId: 15,
+        locationName: 'Whistlin Willys',
+        locationType: 'Pizza Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'16': {
+    locationId: 16,
+        locationName: 'El Maguey',
+        locationType: 'Mexican Restaurant',
+        closeIndicator: true,
+        closeDate: '',
+        locationAddress: ''
+    },
+'17': {
+    locationId: 17,
+        locationName: 'Bobbys PLace',
+        locationType: 'Bar and Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'18': {
+    locationId: 18,
+        locationName: 'Bo Fung',
+        locationType: 'Chinese Food Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+'19': {
+    locationId: 19,
+        locationName: 'Three Kings',
+        locationType: 'English Pub',
+        closeIndicator: true,
+        closeDate: '',
+        locationAddress: ''
+    },
+'20': {
+    locationId: 20,
+        locationName: 'Smoking Barrels',
+        locationType: 'BBQ Restaurant',
+        closeIndicator: false,
+        closeDate: '',
+        locationAddress: ''
+    },
+}];
---
+++ server/data/mockReviews.js
@@ -0,0 +1,173 @@
+const reviews = [{
+ '1': {   
+    reviewId: '1',
+    contactId: '1',
+    locationId: '2',
+    isReviewed: true,
+    rating: 2,
+    reviewDate: 'January 21, 2020',
+    reviewComments: 'Total hole in the wall. The bathroom was something out of a horror movie! But the drinks were cheap and people watching was top notch. '
+},
+'2': {   
+    reviewId: '2',
+    contactId: '5',
+    locationId: '13',
+    isReviewed: true,
+    rating: 5,
+    reviewDate: 'March 2, 2021',
+    reviewComments: 'The steaks were huge and cooked to perfection. The sides and appetizers were equally delicious. A little pricey but worth it!'
+},
+'3': {   
+    reviewId: '3',
+    contactId: '1',
+    locationId: '19',
+    isReviewed: true,
+    rating: 3,
+    reviewDate: 'January 21, 2020',
+    reviewComments: 'Food was ok. Cool patio. Service was a little slow.'
+},
+'4': {   
+    reviewId: '4',
+    contactId: '4',
+    locationId: '11',
+    isReviewed: true,
+    rating: 3,
+    reviewDate: 'July 21, 2021',
+    reviewComments: 'A step above the average hole in the wall. Plenty of bar games and even limited food. A little grimey but a decent atmosphere and karaoke on the weekends! '
+},
+'5': {   
+    reviewId: '5',
+    contactId: '2',
+    locationId: '1',
+    isReviewed: true,
+    rating: 5,
+    reviewDate: 'August 11, 2020',
+    reviewComments: 'Great food, great service. Perfect for special occasions.'
+},
+'6': {   
+    reviewId: '6',
+    contactId: '4',
+    locationId: '3',
+    isReviewed: true,
+    rating: 3,
+    reviewDate: 'April 20, 2021',
+    reviewComments: 'Below average service. Food and drinks took forever. However the food was great when it did finally arrive.'
+},
+'7': {   
+    reviewId: '7',
+    contactId: '3',
+    locationId: '6',
+    isReviewed: true,
+    rating: 2,
+    reviewDate: 'June 1, 2020',
+    reviewComments: 'This place was an absolute mess. Somehow they were out of fish! The wait staff was pestered by unruly customers, one group even tied their servers shoelaces together.'
+},
+'8': {   
+    reviewId: '8',
+    contactId: '5',
+    locationId: '10',
+    isReviewed: true,
+    rating: 5,
+    reviewDate: 'January 21, 2020',
+    reviewComments: `the best Chinese food i've had in recent memory. The orange chicken was crispy and hot.`
+},
+'10': {   
+    reviewId: '10',
+    contactId: '2',
+    locationId: '16',
+    isReviewed: true,
+    rating: 4,
+    reviewDate: 'August 28, 2021',
+    reviewComments: 'The patio was amazing! The wait staff was so friendly and the food was out quick and delicious!'
+},
+'11': {   
+    reviewId: '11',
+    contactId: '5',
+    locationId: '4',
+    isReviewed: true,
+    rating: 1,
+    reviewDate: 'March 17, 2021',
+    reviewComments: 'We went here for St. Pattys Day. It was overrun with drunk people. Trying to get a drink was like entering a mosh pit. Not to mention all of the fluids on the floor, which I tell myself was just spilled beer.'
+},
+'12': {   
+    reviewId: '12',
+    contactId: '3',
+    locationId: '14',
+    isReviewed: true,
+    rating: 5,
+    reviewDate: 'May 5, 2020',
+    reviewComments: 'We went on Cinco De Mayo. For the place being absolutely slammed the staff was incredible! At times the drinks took a little while but that was to be expected. The kitchen was actually pretty quick as well and the quality was not sacrificed.'
+},
+'13': {   
+    reviewId: '13',
+    contactId: '4',
+    locationId: '9',
+    isReviewed: true,
+    rating: 4,
+    reviewDate: 'November 21, 2020',
+    reviewComments: 'I can see why this place is so famous! The pizza was amazing and only a slice or two is all you need if you get the Chicago style. The wait is a little long, but we knew that going in.'
+},
+'14': {   
+    reviewId: '14',
+    contactId: '1',
+    locationId: '12',
+    isReviewed: true,
+    rating: 2,
+    reviewDate: 'February 9, 2021',
+    reviewComments: 'All I can say is wow! This place is the definition of a hole in the wall. The pool table takes up almost all of the space in the joint. The bartender was super friendly but didnt take any sass. No complex drink orders here!'
+},
+'15': {   
+    reviewId: '15',
+    contactId: '3',
+    locationId: '7',
+    isReviewed: true,
+    rating: 5,
+    reviewDate: 'June 12, 2020',
+    reviewComments: `This was the best BBQ I've ever had! The burnt ends toasted ravioli was to die for!`
+},
+'16': {   
+    reviewId: '16',
+    contactId: '1',
+    locationId: '20',
+    isReviewed: true,
+    rating: 1,
+    reviewDate: 'January 2, 2022',
+    reviewComments: `The food was good, but I'm pretty sure I got food poisoning from here. I've eaten here many times, but not sure if I will again!`
+},
+'17': {   
+    reviewId: '1',
+    contactId: '5',
+    locationId: '17',
+    isReviewed: true,
+    rating: 3,
+    reviewDate: 'July 25, 2021',
+    reviewComments: 'Food was good but nothing special. The prices were fair. This place is just fine if looking to get out of the house but nothing really to write home about.'
+},
+'18': {   
+    reviewId: '18',
+    contactId: '1',
+    locationId: '8',
+    isReviewed: true,
+    rating: 5,
+    reviewDate: 'September 21, 2020',
+    reviewComments: 'Amazing Cheese Steaks. Enough said!'
+},
+'19': {   
+    reviewId: '1',
+    contactId: '2',
+    locationId: '18',
+    isReviewed: true,
+    rating: 4,
+    reviewDate: 'May 21, 2020',
+    reviewComments: 'My favorite Chinese food delivery place! The food is always on time or earlier and so fresh!'
+},
+'20': {   
+    reviewId: '1',
+    contactId: '1',
+    locationId: '15',
+    isReviewed: true,
+    rating: 4.5,
+    reviewDate: 'December 6, 2020',
+    reviewComments: 'The favorite place of the kids in town. We are always there for birthdays or after the big little league game. Theres even a mascot which the kids cant get enough of!'
+},
+}]; 
